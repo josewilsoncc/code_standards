8f5713987467c7cde7b7766055847fd63f8d723b
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2017-10-05 16:22:00 -0500

    U

--- Classes/ExampleClass.php
+++ Classes/ExampleClass.php
@@ -1,21 +1,26 @@
 <?php
 
 /*
- * This file is part of the php code standards.
+ * Ejemplo de codificación estandar
  *
  * Jose Capera <josewilsoncc@gmail.com>
  *
  */
 
+#El namespace debe de estar relacionado con la ubicación de la clase.
 namespace Classes;
 
 class ExampleClass {
-    
+
     const SOME_CONSTANT_CLASS = 'SOME_CONSTANT_CLASS';
     /**
      * @var string
      */
     private $someVar;
+
+    function __construct()
+    {
+    }
 
     public function getSomeVar() : string {
         return $this->someVar;
