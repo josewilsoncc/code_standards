3983c1a62fae5efcd266343b9a7b7ad523e724d1
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2017-10-05 09:06:06 -0500

    U

---
+++ Classes/ExampleClass.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * This file is part of the php code standards.
+ *
+ * Jose Capera <josewilsoncc@gmail.com>
+ *
+ */
+
+namespace Classes;
+
+class ExampleClass {
+    const SOME_CONSTANT_CLASS = 'SOME_CONSTANT_CLASS';
+}
