691892a9ca68e78699f7bda637f77bf47fcb39b1
Jose Wilson Capera Castaño <josewilsoncc@gmail.com>
2017-10-13 15:42:23 -0500

    U

--- Classes/ExampleClass.php
+++ Classes/ExampleClass.php
@@ -14,7 +14,12 @@ class ExampleClass {
 
     #Las constantes siempre van al principio de la clase, nombradas con mayúscula y guión bajo entre palabras.
     const SOME_CONSTANT_CLASS = 'SOME_CONSTANT_CLASS';
-    
+
+    /*
+     * Debido a que los tipos especificos para las varibles de clase estan incluidos a partir de PHP 7.1, si se quiere
+     * que una variable sea de tipo String por ejemplo, se debe de declarar como privada y forzarla en los metodos set y
+     * get de dicha variable, para este ejemplo: getSomeVar() y setSomeVar(string $someVar).
+     */
     private $someVar;
 
     function __construct()
